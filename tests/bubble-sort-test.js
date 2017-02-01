b76ad876d787a501fcc071b19d7cf7fe4038693a
Michael Fenwick <mickyfen17@aol.com>
2017-01-31 17:18:42 -0700

    add first passing function and 4 tests

--- tests/bubble-sort-test.js
+++ tests/bubble-sort-test.js
@@ -1,16 +1,23 @@
-const chai   = require('chai');
-const assert = chai.assert;
-
-const bubbleSort = require("../scripts/bubble-sort");
+import { expect } from 'chai';
+import bubbleSort from "../scripts/bubble-sort";
 
 describe("testing Bubble Sort", function() {
-  //
-  // it("should be a function", function() {
-  //   assert.isFunction(bubbleSort);
-  // });
-  it("should check for a output", function() {
-    bubbleSort([1,2,3,4,5,6]).then((value) =>
-      assert.equal(value, [1,2,3,4,5,6])
-    );
+
+  it('should return to me an array', () => {
+    expect(Array.isArray(bubbleSort([]))).to.be.true;
   });
+
+  it("should check for a output", () => {
+    expect(bubbleSort([1,2,3])).to.deep.equal([1,2,3]);
+  });
+
+  it("should filter an unordered array of numbers", () => {
+    expect(bubbleSort([4,2,0,3,1])).to.deep.equal([0,1,2,3,4]);
+  });
+
+  it("should filter an unordered array of letters", () => {
+    expect(bubbleSort(["e","c","a","d","b"])).to.deep.equal(["a","b","c","d","e"]);
+  });
+
+
 });
