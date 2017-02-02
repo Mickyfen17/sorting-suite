bcc626b6b3759f4098e0412d2e2a983b38e51d9e
Michael Fenwick <mickyfen17@aol.com>
2017-02-02 10:57:12 -0700

    remove commented out code

--- scripts/insertion-sort.js
+++ scripts/insertion-sort.js
@@ -10,16 +10,4 @@ const insertionSort = arr => {
   return arr;
 };
 
-// const insertionSort = arr => {
-//   arr.forEach((value, i) => {
-//     let sorted = arr[i];
-//     let unsorted = arr[i+1];
-//     if(sorted > unsorted) {
-//       [arr[i], arr[i+1]] = [arr[i+1], arr[i]];
-//       insertionSort(arr);
-//     }
-//   });
-//   return arr;
-// };
-
 export default insertionSort;
